df0c4dcf80c13511c02751ecf9f0bdadb6b6ac81
Alex Chalakov <a.chalakov@codefoundries.com>
2016-03-07 01:48:27 -0800

    Started the process of modifying the dataloader functions from IN implementation to separate queries

--- data/lib/da_cassandra/ObjectPersister.js
+++ data/lib/da_cassandra/ObjectPersister.js
@@ -1,31 +1,24 @@
 /* @flow weak */
 
-import { runQuery, Uuid } from '../../da_cassandra/_client.js';
+import { runQuery, runQueryOneResult, runQueryNoResult, Uuid } from '../../da_cassandra/_client.js';
 
 // For in operator syntax in CQL:
 // http://stackoverflow.com/questions/23874137/cassandra-in-clause-on-index
 // http://mechanics.flite.com/blog/2014/01/08/the-in-operator-in-cassandra-cql/
+// The approach using IN has limitations, might get changed to batching of queries.
 
-export function ObjectPersister_get( entityName, ObjectType, fieldName, values : Array<any> )
+export function ObjectPersister_get( entityName: string, ObjectType: any, fieldName: string, values : Array<any> )
 {
-  let cqlText = 'SELECT * FROM "' + entityName + '" WHERE "' + fieldName + '" IN (';
-  let cqlTextParams = [ ];
-  let cqlParams = [ ];
+  let cqlText = 'SELECT * FROM "' + entityName + '" WHERE "' + fieldName + '" = ?;';
+  let resultPromises = [ ];
 
   for( let value of values )
-  {
-    cqlTextParams.push( '?' );
-    cqlParams.push( value );
-  }
+    resultPromises.push( runQueryOneResult( ObjectType, cqlText, [ value ] ) );
 
-  cqlText += cqlTextParams.join( ',' ) + ');';
-
-  console.log( 'ObjectPersister_get: ' + cqlText + ', param: ' + values.join( ) );
-
-  return runQuery( ObjectType, cqlText, cqlParams );
+  return Promise.all( resultPromises );
 }
 
-export function ObjectPersister_getList( entityName, ObjectType, fieldName, values : Array<any> )
+export function ObjectPersister_getList( entityName: string, ObjectType: any, fieldName: string, values : Array<any> )
 {
   let cqlText = 'SELECT * FROM "' + entityName + '" WHERE "' + fieldName + '" IN (';
   let cqlTextParams = [ ];
@@ -38,8 +31,6 @@ export function ObjectPersister_getList( entityName, ObjectType, fieldName, valu
   }
 
   cqlText += cqlTextParams.join( ',' ) + ') ALLOW FILTERING;';
-
-  console.log( 'ObjectPersister_getList: ' + cqlText + ', param: ' + values.join( ) );
 
   return runQuery( ObjectType, cqlText, cqlParams )
   .then( arrObjects => {
@@ -74,29 +65,68 @@ export function ObjectPersister_getList( entityName, ObjectType, fieldName, valu
       if( bucketForID == null )
         bucketForID = [ ];
 
-      let dataLoaderValue = { data: bucketForID };
-      dataLoaderValue[ fieldName ] = fieldValue;
-
-      arrResult.push( dataLoaderValue );
+      arrResult.push( bucketForID );
     }
-
-    console.log( '=====-----===== 1 = ' + JSON.stringify( arrResult ) );
 
     return arrResult;
   } )
 }
 
-export function ObjectPersister_add( entityName, entity )
+export function ObjectPersister_add( entityName: string, fields: any )
 {
-  throw new Error( "Not supported" );
+  const id = Uuid.random( );
+
+  let cqlTextFieldNames = 'id';
+  let cqlTextFieldValues = '?';
+  let cqlParams = [
+    id,
+  ];
+
+  for( let fieldName in fields )
+  {
+    cqlTextFieldNames += ', "' + fieldName + '"';
+    cqlTextFieldValues += ', ?';
+    cqlParams.push( fields[ fieldName ] );
+  }
+
+  let cqlText = 'INSERT INTO "' + entityName + '" (' + cqlTextFieldNames + ') VALUES (' + cqlTextFieldValues + ');';
+
+  return runQueryNoResult( cqlText, cqlParams )
+  .then( ( ) => {
+    return id;
+  } )
+  ;
 }
 
-export function ObjectPersister_update( entityName, entity )
+export function ObjectPersister_update( entityName: string, fields: any )
 {
-  throw new Error( "Not supported" );
+  let cqlText = 'UPDATE "' + entityName + '" SET ';
+  let cqlParams = [ ];
+
+  let followingItem = false;
+
+  for( let fieldName in fields )
+    if( fieldName != 'id' ) // Do not update id
+    {
+      if( followingItem )
+        cqlText += ', ';
+      else
+        followingItem = true;
+
+      cqlText += '"' + fieldName + '" = ?';
+      cqlParams.push( fields[ fieldName ] );
+    }
+
+  cqlText += ' WHERE id = ?;';
+  cqlParams.push( id );
+
+  return runQueryNoResult( cqlText, cqlParams );
 }
 
-export function ObjectPersister_delete( entityName, entity )
+export function ObjectPersister_delete( entityName: string, id : Uuid )
 {
-  throw new Error( "Not supported" );
+  const cqlText = 'DELETE FROM "' + entityName + '" WHERE id = ?;';
+  const cqlParams = [ id ];
+
+  return runQueryNoResult( cqlText, cqlParams );
 }
