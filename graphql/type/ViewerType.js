dc60f8a0f6e1cd58ebc90c8d9abb79a8e23ff8be
Alex Chalakov <a.chalakov@codefoundries.com>
2016-03-18 02:16:17 -0700

    Move viewer type fields into units. Close #154

--- graphql/type/ViewerType.js
+++ graphql/type/ViewerType.js
@@ -1,21 +1,20 @@
 /* @flow weak */
 
-import { globalIdField } from "graphql-relay";
 import { GraphQLID, GraphQLBoolean, GraphQLInt, GraphQLString, GraphQLObjectType } from "graphql";
-import { fromGlobalId, connectionArgs, connectionFromArray } from "graphql-relay";
+import { globalIdField, fromGlobalId, connectionArgs, connectionFromArray } from "graphql-relay";
 
-
-import CompendiumsConnection from "../../units/imrsk-example-compendium/graphql/type/CompendiumsConnection";
-import Compendium_getListOrCreate from "../../units/imrsk-example-compendium/graphql/helper/Compendium_getListOrCreate";
+import Compendium_ViewerFields from "../../units/imrsk-example-compendium/graphql/type/Compendium_ViewerFields";
+import Ensayo_ViewerFields from "../../units/imrsk-example-ensayo/graphql/type/Ensayo_ViewerFields";
 import NodeInterface from "../interface/NodeInterface";
-import EnsayosConnection from "../../units/imrsk-example-ensayo/graphql/type/EnsayosConnection";
-import EnsayoType from "../../units/imrsk-example-ensayo/graphql/type/EnsayoType";
 import ToDosConnection from "../../units/imrsk-example-todo/graphql/type/ToDosConnection";
-import TranslaticiarumsConnection from "../../units/imrsk-example-translaticiarum/graphql/type/TranslaticiarumsConnection";
+import ToDo_ViewerFields from "../../units/imrsk-example-todo/graphql/type/ToDo_ViewerFields";
+import Translaticiarum_ViewerFields from "../../units/imrsk-example-translaticiarum/graphql/type/Translaticiarum_ViewerFields";
 import User from '../../data/model/User';
 import { Uuid } from '../../data/lib/CassandraClient.js';
 
+
 const Uuid_0 = Uuid.fromString( '00000000-0000-0000-0000-000000000000' );
+
 
 export default new GraphQLObjectType( {
   name: 'Viewer',
@@ -35,67 +34,9 @@ export default new GraphQLObjectType( {
 
     // <-<-<- User properties
 
-    // ->->-> Compendium access through user
-
-    compendiums: {
-      type: CompendiumsConnection.connectionType,
-      args: { ...connectionArgs },
-      resolve: ( obj, { ...args }, { rootValue: {user_id, objectManager} } ) => Compendium_getListOrCreate( user_id, objectManager ).then( ( arr ) => connectionFromArray( arr, args ) )
-    },
-
-    // <-<-<- Compendium access through user
-
-    // ->->-> Ensayo access through user
-
-    Ensayos: {
-      type: EnsayosConnection.connectionType,
-      args: { ...connectionArgs },
-      resolve: ( obj, { ...args }, { rootValue: {user_id, objectManager} } ) => objectManager.getListBy( 'Ensayo', 'Ensayo_User_id', user_id.toString( ) ).then( ( arr ) => connectionFromArray( arr, args ) )
-    },
-    Ensayo: {
-      type: EnsayoType,
-      args: { ...{ id: { type: GraphQLID } } },
-      resolve: ( parent, { id }, { rootValue: {objectManager} } ) => objectManager.getOneById( 'Ensayo', fromGlobalId(id).id ),
-    },
-
-    // <-<-<- Ensayo access through user
-
-    // ->->-> ToDo access through user
-
-    ToDos: {
-      type: ToDosConnection.connectionType,
-      args: {
-        status: {
-          type: GraphQLString,
-          defaultValue: 'any',
-        },
-        ...connectionArgs,
-      },
-      resolve: ( obj, { status, ...args }, { rootValue: {user_id, objectManager} } ) => {
-        //Filter for: status
-        return objectManager.getListBy( 'ToDo', 'ToDo_User_id', user_id.toString( ) )
-        .then( ( arr ) => connectionFromArray( arr.filter( a_ToDo => status === 'any' || ( a_ToDo.ToDo_Complete === ( status === 'completed' ) ) ), args ) )
-      }
-    },
-    ToDo_TotalCount: {
-      type: GraphQLInt,
-      resolve: ( obj, { ...args }, { rootValue: {user_id, objectManager} } ) => objectManager.getListBy( 'ToDo', 'ToDo_User_id', user_id.toString( ) ).then( ( arr ) => arr.length )
-    },
-    ToDo_CompletedCount: {
-      type: GraphQLInt,
-      resolve: ( obj, { ...args }, { rootValue: {user_id, objectManager} } ) => objectManager.getListBy( 'ToDo', 'ToDo_User_id', user_id.toString( ) ).then( ( arr ) => arr.filter( a_ToDo => a_ToDo.ToDo_Complete ).length )
-    },
-
-    // <-<-<- ToDo access through user
-
-    // ->->-> Translaticiarum access through user
-
-    Translaticiarums: {
-      type: TranslaticiarumsConnection.connectionType,
-      args: { ...connectionArgs },
-      resolve: ( obj, { ...args }, { rootValue: {user_id, objectManager} } ) => objectManager.getListBy( 'Translaticiarum', 'Translaticiarum_User_id', user_id.toString( ) ).then( ( arr ) => connectionFromArray( arr, args ) )
-    },
-
-    // <-<-<- Translaticiarum access through user
+    ...Compendium_ViewerFields,
+    ...Ensayo_ViewerFields,
+    ...ToDo_ViewerFields,
+    ...Translaticiarum_ViewerFields,
   },
 } );
