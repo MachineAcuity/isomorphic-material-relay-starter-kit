01ff2e687abf0253ae690591db418b74fc232ceb
Alex Chalakov <a.chalakov@codefoundries.com>
2016-04-04 23:00:14 -0700

    Upgrade to new version of country flags once it is updated for MUI 0.15. Close #198

--- units/starter-kit-example-mui/webapp/components/MUI_Icons_CountryFlags.jsx
+++ units/starter-kit-example-mui/webapp/components/MUI_Icons_CountryFlags.jsx
@@ -10,10 +10,15 @@ import Divider from 'material-ui/lib/divider';
 import ListItem from 'material-ui/lib/lists/list-item';
 
 import {
+  Icon_Flag_AU,
   Icon_Flag_BG,
+  Icon_Flag_BR,
+  Icon_Flag_CA,
+  Icon_Flag_CN,
   Icon_Flag_DE,
   Icon_Flag_FR,
   Icon_Flag_IN,
+  Icon_Flag_RU,
   Icon_Flag_US
 } from 'material-ui-country-flags';
 
@@ -25,15 +30,25 @@ class MUI_Icons_CountryFlags extends React.Component
       <div>
         <Card>
           <List>
-            <ListItem key="0" primaryText="Icon_Flag_BG" leftIcon={<Icon_Flag_BG />} />
+            <ListItem key="AU" primaryText="Icon_Flag_AU" leftIcon={<Icon_Flag_AU />} />
             <Divider inset={true} />
-            <ListItem key="1" primaryText="Icon_Flag_DE" leftIcon={<Icon_Flag_DE />} />
+            <ListItem key="BG" primaryText="Icon_Flag_BG" leftIcon={<Icon_Flag_BG />} />
             <Divider inset={true} />
-            <ListItem key="2" primaryText="Icon_Flag_FR" leftIcon={<Icon_Flag_FR />} />
+            <ListItem key="BR" primaryText="Icon_Flag_BR" leftIcon={<Icon_Flag_BR />} />
             <Divider inset={true} />
-            <ListItem key="3" primaryText="Icon_Flag_IN" leftIcon={<Icon_Flag_IN />} />
+            <ListItem key="CA" primaryText="Icon_Flag_CA" leftIcon={<Icon_Flag_CA />} />
             <Divider inset={true} />
-            <ListItem key="4" primaryText="Icon_Flag_US" leftIcon={<Icon_Flag_US />} />
+            <ListItem key="CN" primaryText="Icon_Flag_CN" leftIcon={<Icon_Flag_CN />} />
+            <Divider inset={true} />
+            <ListItem key="DE" primaryText="Icon_Flag_DE" leftIcon={<Icon_Flag_DE />} />
+            <Divider inset={true} />
+            <ListItem key="FR" primaryText="Icon_Flag_FR" leftIcon={<Icon_Flag_FR />} />
+            <Divider inset={true} />
+            <ListItem key="IN" primaryText="Icon_Flag_IN" leftIcon={<Icon_Flag_IN />} />
+            <Divider inset={true} />
+            <ListItem key="RU" primaryText="Icon_Flag_RU" leftIcon={<Icon_Flag_RU />} />
+            <Divider inset={true} />
+            <ListItem key="US" primaryText="Icon_Flag_US" leftIcon={<Icon_Flag_US />} />
           </List>
         </Card>
       </div>
